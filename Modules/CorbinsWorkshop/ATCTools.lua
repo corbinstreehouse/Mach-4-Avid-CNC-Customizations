ef1b0fec5f688e6078657488745d11bfb297f718
corbin dunn <corbin@corbinstreehouse.com>
2023-02-24 11:28:16 -0800

    Update ATCTools.lua

--- Modules/CorbinsWorkshop/ATCTools.lua
+++ Modules/CorbinsWorkshop/ATCTools.lua
@@ -7,3 +7,25 @@
 -- Files/Prodcuts: https://www.corbinsworkshop.com
 -- (c) 2023 Corbin Dunn
 -- Software provided as-is. For redistribution rights, please contact me.
+
+local ATCTools = {}
+
+package.path = package.path .. ";./Modules/CorbinsWorkshop/?.lua"
+local ToolForks = require 'ToolForks'
+
+
+function ATCTools.Initialize()
+    ToolForks.Log("Initializing ATCTools")
+
+    
+
+end
+
+
+
+
+
+
+
+
+return ATCTools
