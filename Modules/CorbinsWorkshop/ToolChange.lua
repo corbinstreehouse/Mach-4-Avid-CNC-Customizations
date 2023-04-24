0f6f7bbb81b7882e80f17f2ac0301cc874d188f2
corbin dunn <corbin@corbinstreehouse.com>
2023-04-24 15:41:14 -0700

    The waits are required...did some testing.

--- Modules/CorbinsWorkshop/ToolChange.lua
+++ Modules/CorbinsWorkshop/ToolChange.lua
@@ -166,12 +166,13 @@ function ToolChange.PutToolBackInForkAtPosition(toolForkPosition)
 
 	-- Dwell for a brief moment; if the user e-stops the above movements, we will sometimes execute the next line.
 	-- we don't want to drop the tool, so a quick dwell will throw an exception if we are now in an eStop state
-	MCCntlGcodeExecuteWait("G04 P%.4f", 0.1) -- corbin, try removing
+	MCCntlGcodeExecuteWait("G04 P%.4f", 0.1)
 
 	ToolChange.OpenDrawBar()
 
-	-- give the tool a brief moment to pop out
-	MCCntlGcodeExecuteWait("G04 P%.4f", 0.3) -- corbin, try removing..
+	-- give the tool a brief moment to pop out. I tried it without this, and it almost pulled my fork off.
+	-- my machine takes a bit for the tool to pop out.
+	MCCntlGcodeExecuteWait("G04 P%.4f", 0.3) 
 
 	-- Tool is released; raise the spindle up to the clearance height
 	local ZClearanceWithNoTool = ToolForks.GetZClearanceWithNoTool()
