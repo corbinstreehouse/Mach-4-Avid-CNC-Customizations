877469390c8a5f88d62afb929b427caa3afc0bb3
corbin dunn <corbin@corbinstreehouse.com>
2023-05-18 14:09:07 -0700

    Fix the make release script to not have the hidden files...

--- MakeRelease.sh
+++ MakeRelease.sh
@@ -4,6 +4,11 @@ mkdir -p ./Mach4Hobby
 cp -r ./Modules/ ./Mach4Hobby/Modules/
 mkdir -p ./Mach4Hobby/Profiles/AvidCNC/Macros
 mkdir -p ./Mach4Hobby/Screens
+rm ./Mach4Hobby/Modules/CorbinsWorkshop/.DS_Store
+rm ./Mach4Hobby/Modules/CorbinsWorkshop/.vscode/settings.json 
+rmdir ./Mach4Hobby/Modules/CorbinsWorkshop/.vscode/
+rmdir ./Mach4Hobby/Modules/.vscode/
+rm ./Mach4Hobby/Modules/.DS_Store
 cp ./Profiles/AvidCNC/Macros/m5.mcs ./Mach4Hobby/Profiles/AvidCNC/Macros/m5.mcs
 cp ./Profiles/AvidCNC/Macros/m3.mcs ./Mach4Hobby/Profiles/AvidCNC/Macros/m3.mcs
 cp ./Profiles/AvidCNC/Macros/m6.mcs ./Mach4Hobby/Profiles/AvidCNC/Macros/m6.mcs
